36eb76b088e989c95308ec39104ff223eaf54563
vjy <vijay@iavian.com>
2025-09-29 15:51:27 +0530

    Initial Commit

---
+++ dashy/dashyApp.swift
@@ -0,0 +1,17 @@
+//
+//  dashyApp.swift
+//  dashy
+//
+//  Created by iavian on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct dashyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
